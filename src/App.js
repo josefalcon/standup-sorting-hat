cd32b04bd01a4bd91b3c6690cd86c30e63423084
Jose Falcon <jfalcon@squarespace.com>
2020-07-10 17:23:43 -0400

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import team from './team.json';
+import hat from './sortinghat.svg';
+
+
+const StyledApp = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background: ${(props) => props.sorted ? 'gold': 'white'};
+  transition: background-color 1s ease;
+`;
+
+const StyledResult = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 4em;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledName = styled.div`
+`;
+
+const StyledHat = styled.img`
+  width: 100px;
+  cursor: pointer;
+
+  &:hover {
+    animation: spin 1s infinite;
+  }
+
+  @keyframes spin {
+    from {transform:rotate(0deg);}
+    to {transform:rotate(360deg);}
+  }
+`;
+
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
+
+async function sleep(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, ms);
+  });  
+}
+
+function Result({ person, sorted }) {
+  function pickColor() {
+    return Math.floor(Math.random()*16777215).toString(16);
+  }
+
+  const styles = {
+    color: sorted ? 'white' : `#${pickColor()}`,
+  };
+
+  return (
+    <StyledResult>
+      <div style={{ whiteSpace: "pre" }}>
+        {"You're a wizard"}
+      </div>
+      <StyledName style={styles}>
+        {person}
+      </StyledName>
+    </StyledResult>
+  );
+}
+
 
 function App() {
+  const [selectedPerson, setSelectedPerson] = useState(undefined);
+  const [sorted, setSorted] = useState(false);
+
+  function pickSomeone() {
+    return team.members[Math.floor(Math.random() * team.members.length)];
+  }
+
+  async function handleSort() {
+    shuffle(team.members);
+    
+    setSorted(false);
+    for (let i = 0; i < 40; i++) {
+      setSelectedPerson(pickSomeone());
+      await sleep(30 * (Math.sqrt(i)));
+    }
+    setSorted(true);
+  }  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledApp sorted={sorted}>
+      <StyledHat src={hat} onClick={handleSort} />
+      {selectedPerson && (
+        <Result person={selectedPerson} sorted={sorted}/>
+      )}
+    </StyledApp>
   );
 }
 
