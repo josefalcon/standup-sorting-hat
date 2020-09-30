0740836ee5485985db1adc53c017077c33e8f38f
Jose Falcon <jfalcon@squarespace.com>
2020-09-30 11:38:24 -0400

    Updates

---
+++ precache-manifest.dd7c3c40ab02bf0ea8d9aba2182628be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a77d9c41be1474c1b27b1b9368aa197",
+    "url": "/standup-sorting-hat/index.html"
+  },
+  {
+    "revision": "ada43f6b86a6b189f306",
+    "url": "/standup-sorting-hat/static/css/main.7b9d120d.chunk.css"
+  },
+  {
+    "revision": "1b41afc08c2c1390a2bb",
+    "url": "/standup-sorting-hat/static/js/2.8b6f1487.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/standup-sorting-hat/static/js/2.8b6f1487.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada43f6b86a6b189f306",
+    "url": "/standup-sorting-hat/static/js/main.a86aa1e6.chunk.js"
+  },
+  {
+    "revision": "6fa1c77cde3618d31410",
+    "url": "/standup-sorting-hat/static/js/runtime-main.432f5c57.js"
+  },
+  {
+    "revision": "181ef9a7aee45c119e68f294e8426a3a",
+    "url": "/standup-sorting-hat/static/media/harry_potter.181ef9a7.ttf"
+  },
+  {
+    "revision": "c17c5207e7023d7da69886aa5fb2d22e",
+    "url": "/standup-sorting-hat/static/media/sortinghat.c17c5207.svg"
+  }
+]);
